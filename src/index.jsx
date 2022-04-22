110907ea318f8b54b02df605286af8845d80699b
Gowtham T G <gowthamgopinath1@gmail.com>
2022-04-22 20:54:20 +0530

    Gokul Files Added

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import "./styles/index.scss";
 import "./styles/sass/index.scss";
@@ -7,12 +7,22 @@ import "./styles/sass/index.scss";
 // import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { ThemeProvider } from "./Context";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+ReactDOM.render(
+  <ThemeProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ThemeProvider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import Navbar from "../components/landingPage/Navbar/Navbar";
+import Intro from "../components/landingPage/Intro/Intro";
+import Services from "../components/landingPage/Services/Services";
+import "../App.css";
+import Experience from "../components/landingPage/Experience/Experience";
+import Works from "../components/landingPage/Works/Works";
+import Portfolio from "../components/landingPage/Portfolio/Portfolio";
+import Testimonial from "../components/landingPage/Testimonials/Testimonial";
+import Contact from "../components/landingPage/Contact/Contact";
+import Footer from "../components/landingPage/Footer/Footer";
+import { useContext } from "react";
+import { themeContext } from "../Context";
 
-const Home = () => {
-    return <h1>Home</h1>;
-};
+function Home() {
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+  return (
+    <div
+      className="Home"
+      style={{
+        background: darkMode ? "black" : "",
+        color: darkMode ? "white" : "",
+      }}
+    >
+      <Navbar />
+      <Intro />
+      <Services />
+      <Experience />
+      <Works />
+      <Portfolio />
+      <Testimonial />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
 
 export default Home;
