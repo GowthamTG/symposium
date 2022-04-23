3666ad0f7f4d302d1c2ace2450933be1d8f8b980
Gowtham T G <gowthamgopinath1@gmail.com>
2022-04-23 21:28:35 +0530

    Events Page Updated

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -64,7 +64,7 @@ const Header = () => {
               </Link>
             </li>
             <li>
-              <Link to="/page-three" onClick={menuToggleHandler}>
+              <Link to="/events" onClick={menuToggleHandler}>
                 Events
               </Link>
             </li>
--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import EventCard from "../components/card/Card";
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
+// import Carousel from "react-multi-carousel";
+// import "react-multi-carousel/lib/styles.css";
 
+import LeftArrow from "../assets/left-arrow.svg";
+import RightArrow from "../assets/right-arrow.svg";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import super1 from "../assets/super1.png";
 import super2 from "../assets/super2.png";
 import super3 from "../assets/super3.png";
 import super4 from "../assets/super4.png";
 import { Link } from "react-router-dom";
+
+import Grid from "@mui/material/Grid";
 
 const Events = () => {
   const events = [
@@ -43,30 +49,53 @@ const Events = () => {
       tag: 12983,
       time: 1,
     },
+    {
+      image: super4,
+      series: "Gloop Series",
+      title: "Non Technical",
+      price: 2.99,
+      tag: 12983,
+      time: 1,
+    },
   ];
+
+  const SlickArrowLeft = ({ currentSlide, slideCount, ...props }) => (
+    <img src={LeftArrow} alt="prevArrow" {...props} />
+  );
+
+  const SlickArrowRight = ({ currentSlide, slideCount, ...props }) => (
+    <img src={RightArrow} alt="nextArrow" {...props} />
+  );
+
   return (
-    <div className="super-rare">
-      <div className="title-container">
-        <h2 className="page--title">EVENTS</h2>
-        <p className="description">
-          We have released four limited edition NFT's early which which can be
-          bid on via <Link to="">OpenSea</Link>.
-        </p>
+    <>
+      <div className="super-rare">
+        <div className="title-container">
+          <h2 className="page--title">EVENTS</h2>
+          <p className="description">
+            We have released four limited edition NFT's early which which can be
+            bid on via <Link to="">OpenSea</Link>.
+          </p>
+        </div>
+        <div className="event__card--container">
+          <Grid container spacing={1} justify="center">
+            {events.map((event) => (
+              <Grid item xs={12} sm={6} md={3}>
+                <EventCard
+                  key={event.title}
+                  image={event.image}
+                  series={event.series}
+                  title={event.title}
+                  price={event.price}
+                  tag={event.tag}
+                  time={event.time}
+                />
+              </Grid>
+            ))}
+          </Grid>
+        </div>
       </div>
-      <div className="event__cards">
-        {events.map((event) => (
-          <EventCard
-            key={event.title}
-            image={event.image}
-            series={event.series}
-            title={event.title}
-            price={event.price}
-            tag={event.tag}
-            time={event.time}
-          />
-        ))}
-      </div>
-    </div>
+    </>
   );
 };
 
