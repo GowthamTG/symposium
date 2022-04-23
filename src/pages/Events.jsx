a684344ea3ed8073bc7f2496219135199cf3d97e
Gowtham T G <gowthamgopinath1@gmail.com>
2022-04-23 23:17:51 +0530

    Events Page Fixed

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import EventCard from "../components/card/Card";
-// import Carousel from "react-multi-carousel";
-// import "react-multi-carousel/lib/styles.css";
 
 import LeftArrow from "../assets/left-arrow.svg";
 import RightArrow from "../assets/right-arrow.svg";
@@ -13,7 +11,7 @@ import super3 from "../assets/super3.png";
 import super4 from "../assets/super4.png";
 import { Link } from "react-router-dom";
 
-import Grid from "@mui/material/Grid";
+import Slider from "react-slick";
 
 const Events = () => {
   const events = [
@@ -47,7 +45,7 @@ const Events = () => {
       title: "Non Technical",
       price: 2.99,
       tag: 12983,
-      time: 1,
+      time: 2,
     },
     {
       image: super4,
@@ -55,47 +53,92 @@ const Events = () => {
       title: "Non Technical",
       price: 2.99,
       tag: 12983,
-      time: 1,
+      time: 3,
+    },
+    {
+      image: super4,
+      series: "Gloop Series",
+      title: "Non Technical",
+      price: 2.99,
+      tag: 12983,
+      time: 4,
     },
   ];
 
   const SlickArrowLeft = ({ currentSlide, slideCount, ...props }) => (
-    <img src={LeftArrow} alt="prevArrow" {...props} />
+    <div className="carousel__arrows">
+      <img src={LeftArrow} alt="prevArrow" {...props} />
+    </div>
   );
 
   const SlickArrowRight = ({ currentSlide, slideCount, ...props }) => (
-    <img src={RightArrow} alt="nextArrow" {...props} />
+    <div className="carousel__arrows">
+      <img src={RightArrow} alt="nextArrow" {...props} />
+    </div>
   );
-
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+    nextArrow: <SlickArrowRight />,
+    prevArrow: <SlickArrowLeft />,
+  };
   return (
-    <>
-      <div className="super-rare">
-        <div className="title-container">
-          <h2 className="page--title">EVENTS</h2>
-          <p className="description">
-            We have released four limited edition NFT's early which which can be
-            bid on via <Link to="">OpenSea</Link>.
-          </p>
-        </div>
-        <div className="event__card--container">
-          <Grid container spacing={1} justify="center">
+    <div className="super-rare">
+      <div className="title-container">
+        <h2 className="page--title">EVENTS</h2>
+        <p className="description">
+          We have released four limited edition NFT's early which which can be
+          bid on via <Link to="">OpenSea</Link>.
+        </p>
+      </div>
+      <div className="event__cards">
+        <div className="container center">
+          <Slider {...settings}>
             {events.map((event) => (
-              <Grid item xs={12} sm={6} md={3}>
-                <EventCard
-                  key={event.title}
-                  image={event.image}
-                  series={event.series}
-                  title={event.title}
-                  price={event.price}
-                  tag={event.tag}
-                  time={event.time}
-                />
-              </Grid>
+              <EventCard
+                key={event.time}
+                image={event.image}
+                series={event.series}
+                title={event.title}
+                price={event.price}
+                tag={event.tag}
+                time={event.time}
+              />
             ))}
-          </Grid>
+          </Slider>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
